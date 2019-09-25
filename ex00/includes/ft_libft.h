b901155ab18df165ca4c1cf6d6a3234f3444d972
Sander Verheijen <sanderverheijen@outlook.com>
2019-09-25 16:14:59 +0200

    removed unused function ex00

--- ex00/includes/ft_libft.h
+++ ex00/includes/ft_libft.h
@@ -6,15 +6,14 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/23 11:29:27 by averheij       #+#    #+#                */
-/*   Updated: 2019/09/25 15:53:10 by averheij      ########   odam.nl         */
+/*   Updated: 2019/09/25 16:13:19 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LIBFT_H
-#define FT_LIBFT_H
+# define FT_LIBFT_H
 # include <unistd.h>
 
 void	ft_putchar(char c);
 void	ft_putstr(char *str);
-void	ft_putnbr(int n);
 #endif
